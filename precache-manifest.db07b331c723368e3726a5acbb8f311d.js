f835172f0c553e9e53cd7816cb4822d870f76cec
ryancfields <ryancfields@users.noreply.github.com>
2020-08-17 21:03:36 +0000

    Deploying to gh-pages from  @ 4481ebbcaee1416ada13fb8056ca545495b3cd42 🚀

---
+++ precache-manifest.db07b331c723368e3726a5acbb8f311d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fadcf015ee01982eb1b9d1e9c901ca",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "83598c9c87b4c66da40f",
+    "url": "/flashcards/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "26904af4d480c5856d83",
+    "url": "/flashcards/static/css/main.2d32f3ea.chunk.css"
+  },
+  {
+    "revision": "83598c9c87b4c66da40f",
+    "url": "/flashcards/static/js/2.1db7d950.chunk.js"
+  },
+  {
+    "revision": "603d9a3c4cc5b514df9304cf7a18f352",
+    "url": "/flashcards/static/js/2.1db7d950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26904af4d480c5856d83",
+    "url": "/flashcards/static/js/main.df958011.chunk.js"
+  },
+  {
+    "revision": "f37a03f613c770e0327b",
+    "url": "/flashcards/static/js/runtime-main.1412b638.js"
+  }
+]);
