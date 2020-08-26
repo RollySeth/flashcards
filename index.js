1ebbd9249b28ff663c891af7a7c14e682a88813b
Ryan Fields <rcfields@u.washington.edu>
2020-08-26 08:09:52 -0700

    adding mongodb local / production check

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const server = require("./server");
 const mongoose = require("mongoose");
-
 const port = process.env.PORT || 5000;
 
-mongoose
-  .connect("mongodb://localhost/week5", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    server.listen(port, () => {
-      console.log(`Server is listening on http://localhost:${port}`);
+require("dotenv").config({ path: ".env" });
+const { MONGO_DB_CONNECTION_STRING, NODE_ENV } = process.env;
+
+if (NODE_ENV === "production") {
+  console.log("Using Production MongoDB");
+  mongoose
+    .connect(MONGO_DB_CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      server.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+      });
     });
-  });
+} else {
+  console.log("Connecting to local MongoDB");
+  mongoose
+    .connect("mongodb://localhost/flash2020", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      server.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+      });
+    });
+}
