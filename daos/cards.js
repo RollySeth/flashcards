a3c3f091c2e3173335784d1d6e35e002f118f9f4
Tanveer Ali <tanveer.i.ali@gmail.com>
2020-08-27 09:06:07 -0700

    Routes added (#9)
    * login routes created,, start script changed
    
    * commit with react
    
    * adding mongodb local / production check
    
    * cards/cardsets routes complete
    
    * merge conflict
    
    Co-authored-by: Tanveer <tanveera@uw.edu>
    Co-authored-by: Ryan Fields <rcfields@u.washington.edu>

---
+++ daos/cards.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+const Card = require("../models/cards");
+
+module.exports = {};
+
+module.exports.create = async (setId, sideA, sideB) => {
+  try {
+    const cards = await Card.create({
+      setId: setId,
+      sideA: sideA,
+      sideB: sideB,
+    });
+    return cards;
+  } catch (e) {
+    throw e;
+  }
+};
+
+module.exports.updateCardById = async (cardId, setId, sideA, sideB) => {
+  const card = await Card.updateOne(
+    {
+      _id: cardId,
+      setId: setId,
+    },
+    {
+      sideA: sideA,
+      sideB: sideB,
+      cardsAttempts: 0,
+      cardsCorrect: 0,
+    }
+  );
+  if (card) {
+    return card;
+  } else {
+    return false;
+  }
+};
+
+module.exports.addAttempts = async (cardId, setId, num) => {
+  // ifCorrect should be 0 if answer incorrect and 1 if correct
+  const card = await Card.findOne({
+    _id: cardId,
+    setId: setId,
+  });
+  const number = num === "0" ? 0 : 1;
+  cardsAttempts = card.cardsAttempts + 1;
+  cardsCorrect = card.cardsCorrect + parseInt(number);
+  if (card) {
+    const updatedCard = await Card.updateOne(
+      {
+        _id: cardId,
+        setId: setId,
+      },
+      {
+        cardsAttempts: cardsAttempts,
+        cardsCorrect: cardsCorrect,
+      }
+    );
+
+    if (updatedCard) {
+      return updatedCard;
+    } else {
+      return false;
+    }
+  }
+};
+
+module.exports.getById = async (cardId, setId) => {
+  if (!setId) {
+    return null;
+  }
+  return Card.findOne({ _id: cardId, setId: setId }).lean();
+};
+
+module.exports.deleteById = async (cardId) => {
+  if (!cardId) {
+    return null;
+  }
+  await Card.deleteOne({ _id: cardId });
+  return true;
+};
+
+class BadDataError extends Error {}
+module.exports.BadDataError = BadDataError;
--- models/cards.js
+++ models/cards.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const cardSchema = new mongoose.Schema({
-  setId: { type: String, required: true },
   sideA: { type: String, required: true },
   sideB: { type: String, required: true },
   cardsAttempts: { type: Number, default: 0 },
   cardsCorrect: { type: Number, default: 0 },
+  setId: { type: String, required: true, index: true },
 });
 
-module.exports = mongoose.model("card", cardSchema);
+module.exports = mongoose.model("cards", cardSchema);
