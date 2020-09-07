dc70fda2c5f35bf4c0079c3109ffa6191f784944
Tanveer Ali <tanveer.i.ali@gmail.com>
2020-09-07 16:27:54 -0700

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -20,9 +20,7 @@ import { grey } from "@material-ui/core/colors";
 
 //service to send login credential to express backend
 const LoginService = (data) =>
-  axios
-    .post(`${process.env.REACT_APP_BASEURI}/login`, data)
-    .then((res) => res);
+  axios.post(`${process.env.REACT_APP_BASEURI}/login`, data).then((res) => res);
 
 //Email Vaidation service connect express login/emailcheck route to check user entered email ..to not allow duplicate email entried
 const EmailValidation = (data) =>
@@ -41,7 +39,15 @@ class SignIn extends React.Component {
       loginSuccess: false,
     };
   }
-  
+  //   componentDidMount() {
+
+  // //  this.state = {value: this.props.value};
+  //  this.handleOnChangeEmail = this.handleOnChangeEmail.bind(this);
+  //  this.handleOnChangePassword  = this.handleOnChangePassword.bind(this);
+  //  this.handleOnBlur = this.handleOnBlur.bind(this);
+  //  this.onSubmit = this.onSubmit.bind(this);
+
+  //   }
   //called when user moves out of email input field
   handleOnChangeEmail = (e) => {
     this.setState({
@@ -80,14 +86,15 @@ class SignIn extends React.Component {
   // sending login information to express route after submit button is pressed
   onSubmit = async (e) => {
     e.preventDefault();
+
     this.setState({ wrong_password: true });
     const data = {
       email: this.state.email,
       password: this.state.password,
     };
     const loginResult = await LoginService(data);
-    console.log(`Test ${loginResult}`);
-    if (loginResult !== 200) {
+    console.log(`Test ${loginResult.status}`);
+    if (loginResult.status !== 200) {
       console.log("Log in Failed.");
       this.setState({
         loginSuccess: false,
@@ -95,6 +102,7 @@ class SignIn extends React.Component {
       });
     } else {
       console.log("Log in Success.");
+      console.log(JSON.stringify(loginResult.data));
       localStorage.setItem("userData", JSON.stringify(loginResult.data));
       this.setState({
         loginSuccess: true,
@@ -129,7 +137,7 @@ class SignIn extends React.Component {
                 />
               </div>
             </Col>
-            <Col className={`h-100 test`} md={6} xs={12} lg={6} xl={6}>
+            <Col className={`h-100`} md={6} xs={12} lg={6} xl={6}>
               <h7>
                 {" "}
                 <Button href="/flashcards/#/signup">Sign-Up</Button>
@@ -159,13 +167,13 @@ class SignIn extends React.Component {
                 />
               </h5>
               <h6>
-                {/* <Link to="/flashcards/#/home"> */}
+                {/* <Link to="/flashcards/#/new"> */}
                 <Button
                   variant="contained"
                   onClick={this.onSubmit}
                   disabled={sign_up_reqd}
                   color="secondary"
-                  href="/flashcards/#/home"
+                  href="/flashcards/#/new"
                 >
                   Log-In
                 </Button>
@@ -178,7 +186,6 @@ class SignIn extends React.Component {
                   </Alert>
                 )}
                 {!wrong_password && <Alert severity="error"></Alert>}
-
               </h3>
             </Col>
           </Row>
