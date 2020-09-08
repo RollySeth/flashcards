52a0e4eb9f77e122f6ecf1ea09bf614d58b707d0
Tanveer Ali <tanveer.i.ali@gmail.com>
2020-09-07 19:58:13 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -25,7 +25,7 @@ export default class Home extends React.Component {
       .get(
         `${process.env.REACT_APP_BASEURI}/set/search?s=` + this.state.value,
         this.state.headers
-      )
+      
       .then((response) => {
         this.setState({ searchedSets: response.data });
       })
@@ -63,7 +63,7 @@ export default class Home extends React.Component {
           });
         axios
           .get(
-            `${process.env.REACT_APP_BASEURI}/set/public/`,
+            `${process.env.REACT_APP_BASEURI}/set/public`,
             this.state.headers
           )
           .then((response) => {
